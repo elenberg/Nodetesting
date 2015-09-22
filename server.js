37af9fdce5ac4232ada74ea6d6c52d366d59d9c5
root <root@school.batterystapler.com>
2015-09-22 17:33:59 +0000

    nodejs testing

---
+++ server.js
@@ -0,0 +1,39 @@
+var express = require('express');
+
+var app = express();
+var instagram = require('instagram-node').instagram();
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
+instagram.use({
+
+  client_id: 'f81f407862d44b03a130dfb1c020c5ff',
+  client_secret: 'd337b5c6f52f4b3a8270d83c2d88ef18'
+
+});
+
+//app.get('/authorize_user', exports.authorize_user);
+
+//app.get('/handleauth', exports.handleauth);
+
+
+app.get('/', function(req, res) {
+  /////
+  instagram.media_popular(function(err, medias, remaining, limit){
+
+      res.render('public/pages/index.ejs', {gram: medias });
+  });
+
+});
+
+app.listen(8080, function(err){
+  if(err){
+    console.log("Error");
+  }
+  else{
+    console.log("Listening");
+  }
+
+});
