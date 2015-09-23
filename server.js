bb55d6434fcacd9a2a2783e3d1cf500e732314c4
elenberg <everettlenberg@gmail.com>
2015-09-23 00:32:29 -0600

    Possibly authentication testing
    It'd be coooool

--- server.js
+++ server.js
@@ -21,13 +21,56 @@ instagram.use({
 //app.get('/handleauth', exports.handleauth);
 
 
-
 app.get('/', function(req, res) {
 
             instagram.media_popular(function(err, medias, remaining, limit){
             res.render('public/pages/index.ejs', {gram: medias });
   });
 
+});
+
+app.get('/login', function(req, res) {
+
+            instagram.media_popular(function(err, medias, remaining, limit){
+            res.render('public/pages/index.ejs', {gram: medias });
+  });
+
+});
+
+
+app.get('/dashboard', function(req, res) {
+
+            instagram.media_popular(function(err, medias, remaining, limit){
+            res.render('public/pages/index.ejs', {gram: medias });
+  });
+
+});
+
+
+app.get('/search', function(req, res) {
+
+            instagram.media_popular(function(err, medias, remaining, limit){
+            res.render('public/pages/index.ejs', {gram: medias });
+  });
+
+});
+
+
+app.get('/search', function(req, res) {
+
+            instagram.media_popular(function(err, medias, remaining, limit){
+            res.render('public/pages/index.ejs', {gram: medias });
+  });
+
+});
+
+app.get('/?*', function(req, res) {
+
+           var pathname = url.parse(req.url).query;
+           console.log(pathname);
+           instagram.media_popular(function(err, medias, remaining, limit){
+           res.render('public/pages/index.ejs', {gram: medias });
+           });
 });
 /*
 http.createServer(function (request, response) {
